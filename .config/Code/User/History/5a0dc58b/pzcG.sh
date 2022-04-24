c88fce1d28e07550b37b8416266a539ef6c422a1
deuceaj <43860830+deuceaj@users.noreply.github.com>
2022-04-24 11:40:56 -0400

    .config

---
+++ .config/Code/User/History/5a0dc58b/pzcG.sh
@@ -0,0 +1,55 @@
+#######################################
+INSTALL QEMU VirtManager on Arch
+#######################################
+https://computingforgeeks.com/install-kvm-qemu-virt-manager-arch-manjar/
+
+https://christitus.com/vm-setup-in-linux/
+
+    ##################
+    Required Packages
+    ##################
+    archlinux-keyring qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat ebtables iptables libguestfs edk2-ovmf swtpm
+
+
+    #Enable and Start Libvirt
+    sudo systemctl enable libvirtd.service
+    sudo systemctl start libvirtd.service
+
+    #Set default network, autostartm, check status
+    sudo virsh net-start default
+    sudo virsh net-autostart default
+    sudo virsh net-list --all
+
+    #EDIT TO USE LINUX ACCOUNT
+    sudo code /etc/libvirt/libvirtd.conf
+    LINE 81:  unix_sock_group = "libvirt"
+    LINE 104: unix_sock_rw_perms = "0770"
+
+
+    #Add user and new group
+    sudo usermod -a -G libvirt $(whoami)
+    newgrp libvirt
+
+    #Restart Libvirt and set default network
+    sudo systemctl restart libvirtd.service
+
+
+
+
+###################################################
+6) Preparation and placing of ROM file
+###################################################
+https://www.techpowerup.com/vgabios/203050/msi-rx570-8192-180304
+
+sudo mkdir /usr/share/vgabios
+place the rom in above directory with
+cd /usr/share/vgabios
+sudo chmod -R 660 <ROMFILE>.rom
+sudo chown username:username <ROMFILE>.rom
+
+For VirtManager: /usr/share/vgabios/RX570.rom
+
+
+
+
+
